be16405d22d726a1320a6dae21c2a0e74d0bba9e
Govorunov Aleksey <87661707+govorunov-av@users.noreply.github.com>
2025-01-26 21:58:25 +0300

    Add files via upload
    Add delete domain`s security group`s

--- ci_user_mgmt.sh
+++ ci_user_mgmt.sh
@@ -37,8 +37,8 @@ create_users() {
     done < "$user_file"
 
     #Применяем лимиты
-    openstack quota set --cores "$CORES_LIMIT" --ram "$RAM_LIMIT" --gigabytes "$GIGABUTES_LIMIT" --floating-ips "$FLOATING_IPS_LIMIT" "$project_name" $INSECURE
-    echo "Лимиты установлены --cores $CORES_LIMIT --ram $RAM_LIMIT --gigabytes $GIGABUTES_LIMIT --floating-ips $FLOATING_IPS_LIMIT в $project_name"
+    openstack quota set --cores "$CORES_LIMIT" --ram "$RAM_LIMIT" --gigabytes "$GIGABYTES_LIMIT"  --floating-ips "$FLOATING_IPS_LIMIT" "$project_name" $INSECURE
+    echo "Лимиты установлены --cores $CORES_LIMIT --ram $RAM_LIMIT --gigabytes $GIGABYTES_LIMIT --floating-ips $FLOATING_IPS_LIMIT в $project_name"
 }
 
 #Удаление ресурсов пользователя
@@ -83,14 +83,14 @@ delete_user_and_resources() {
     delete_user_resources "$username"
 
     # Удаляем floating IP
-    sleep 5
+    sleep 2
     for id in $(openstack floating ip list --project "$project_name" -c ID -f value $INSECURE); do
         openstack floating ip delete "$id" $INSECURE
         echo "Удалён floating IP: $id"
     done
 
     #Отсоединение портов от роутеров
-    sleep 5
+    sleep 2
     for id in $(openstack router list --project "$project_name" -c ID -f value $INSECURE); do
         openstack router set "$id" --disable $INSECURE
         local router_port=$(openstack router show $id -c interfaces_info -f value $INSECURE | awk -F'"port_id": "' '{for (i=2; i<=NF; i++) print $i}' | cut -d'"' -f1)
@@ -101,37 +101,49 @@ delete_user_and_resources() {
     done
 
     #Удаляем порты
-    sleep 5
+    sleep 2
     for id in $(openstack port list --project "$project_name" -c ID -f value $INSECURE); do
         openstack port delete "$id" $INSECURE 2> /dev/null
         echo "Порт удален: $id"
     done
 
     #Удаляем маршрутизаторы
-    sleep 5
+    sleep 2
     for id in $(openstack router list --project "$project_name" -c ID -f value $INSECURE); do
         openstack router delete "$id" $INSECURE
         echo "Удалён маршрутизатор: $id"
     done
 
     #Удаляем подсети
-    sleep 5
+    sleep 2
     for id in $(openstack subnet list --project "$project_name" -c ID -f value $INSECURE); do
         openstack subnet delete "$id" $INSECURE
         echo "Удалена сеть: $id"
     done
 
     #Удаляем сети
-    sleep 5
+    sleep 2
     for id in $(openstack network list --project "$project_name" -c ID -f value $INSECURE); do
         openstack network delete "$id" $INSECURE
         echo "Удалена сеть: $id"
     done
 
+    #Удаляем группы безопасности
+    sleep 2
+    for id in $(openstack security group list --project "$project_name" -c ID -f value $INSECURE); do
+        openstack security group delete "$id" $INSECURE
+        echo "Удалена группа безопасности: $id"
+    done
+
+    #Удаление пользователя
     openstack user delete "$username" $INSECURE
     echo "Пользователь $username удалён."
+
+    #Удаление проекта
     openstack project delete "$project_name" --domain "$domain_name" $INSECURE
     echo "Проект $project_name удалён."
+
+    #Удаление домена
     openstack domain set --disable "$domain_name" $INSECURE
     sleep 2
     openstack domain delete "$domain_name" $INSECURE
